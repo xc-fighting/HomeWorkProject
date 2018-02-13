60cc540061c7be24715f7b1d170bb48f7ebb72e4
Chen Xu <cxu264@usc.edu>
2018-02-13 00:30:01 -0800

    add basic function in packet thread and bucket,may have problem of busy waiting

--- warmup2.c
+++ warmup2.c
@@ -3,19 +3,26 @@
 #include <time.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
 #include "my402list.h"
-
- int lambda;
- float mu;
- int r;
- int B;
- int P;
- int num;
+/*
+  global variables for time intervals
+*/
+ int lambda=1;
+ float mu=0.35;
+ float  r=1.5;
+ int B=10;
+ int P=3;
+ int num=20;
  char* tsfile;
- int test=0;
+ 
+/*
+  two queue needed for thread
+*/
  My402List Q1;
  My402List Q2;
- My402List bucket;
+ int bucket=0;;
+
  pthread_mutex_t lock=PTHREAD_MUTEX_INITIALIZER;
  
  pthread_t packet_thread;
@@ -23,26 +30,62 @@
  pthread_t server1_thread;
  pthread_t server2_thread;
 
+ typedef struct tagpacket{
+ 	long arrive_time;
+ 	long departure_time;
+ 	int demand;
+ 	int id;
+ }packet;
  void* serverFunction(){
- 	pthread_mutex_lock(&lock);
-    test++;
-    printf("%d\n",test);
- 	pthread_mutex_unlock(&lock);
+ 	while(TRUE){
+ 		pthread_mutex_lock(&lock);
+ 		if(bucket<B){
+ 			bucket++;
+            printf("bucket %d arrives,token bucket now have %d token\n",bucket,bucket);
+ 		}
+        
+ 	    pthread_mutex_unlock(&lock);
+ 	    sleep(2);
+ 	}
+ 	
  	return (void*)NULL;
  }
 
  void* packetFunction(){
- 	pthread_mutex_lock(&lock);
-    test++;
-     printf("%d\n",test);
- 	pthread_mutex_unlock(&lock);
+ 	int i=0;
+ 	int solved=0;
+ 	while(solved<num){
+ 		pthread_mutex_lock(&lock);
+ 		if(i<num){
+ 			packet* item=(packet*)malloc(sizeof(packet));
+            item->id=i+1;
+            item->demand=P;
+            printf("p%d arrives,needs %d,inter-arrival time=%.3fms\n",item->id,item->demand,(float)(1000/lambda));
+            My402ListAppend(&Q1,(void*)item);
+            printf("p%d enters Q1\n",item->id);
+            i++;
+ 		}
+        
+        My402ListElem* head=My402ListFirst(&Q1);
+        packet* temp=(packet*)(head->obj);
+        if(bucket>=temp->demand){
+        	bucket-=temp->demand;
+        	My402ListUnlink(&Q1,head);
+        	solved++;
+            printf("get out of Q1 and bucket size is %d\n",bucket);
+        }
+ 	    pthread_mutex_unlock(&lock);
+ 	    sleep(1);
+
+ 	}
+ 	
  	return (void*)NULL;
  }
 
  void* bucketFunction(){
  	 pthread_mutex_lock(&lock);
-     test++;
-     printf("%d\n",test);
+     
+
  	 pthread_mutex_unlock(&lock);
  	 return (void*)NULL;
  }
@@ -51,11 +94,11 @@
 	 printf("number to arrive = %d\n",num);
 	 printf("lambda = %d\n",lambda);
 	 printf("mu = %f\n",mu);
-	 printf("r = %d\n",r);
+	 printf("r = %f\n",r);
 	 printf("B = %d\n",B);
 	 printf("P = %d\n",P);
-	 printf("num = %d\n",num);
      printf("%s\n",tsfile);
+     printf("emulation begins\n");
  }
  void makeChoice(int argc,char* argv[]){
      if(argc==1){
@@ -70,7 +113,7 @@
                mu=atof(argv[i+1]);
      	 }
      	 else if(strcmp(argv[i],"-r")==0){
-               r=atoi(argv[i+1]);
+               r=atof(argv[i+1]);
      	 }
      	 else if(strcmp(argv[i],"-B")==0){
                B=atoi(argv[i+1]);
@@ -99,7 +142,7 @@ int main(int argc,char* argv[]){
     printParam();
     My402ListInit(&Q1);
     My402ListInit(&Q2);
-    My402ListInit(&bucket);
+    
     pthread_create(&packet_thread,0,serverFunction,(void*)NULL);
     pthread_create(&token_thread,0,packetFunction,(void*)NULL);
     pthread_create(&server1_thread,0,serverFunction,(void*)NULL);
@@ -108,5 +151,6 @@ int main(int argc,char* argv[]){
     pthread_join(token_thread,0);
     pthread_join(server1_thread,0);
     pthread_join(server2_thread,0);
+    printf("emulation ends\n");
 	return 0;
 }
